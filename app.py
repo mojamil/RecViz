5cd367610dac8a86a33c78ca2dbbe12b6f179855
Mohammed Jamil <mjamil90@stuy.edu>
2020-06-11 19:56:46 -0400

    the bokeh module provides a better graphing than the dash app

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import json
 from dash.dependencies import Input, Output
 from drawgraph import fig
 
 app = dash.Dash()
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
-])
+    dcc.Graph(id='basic-interactions',figure=fig),
+    html.Div([
+            dcc.Markdown("""
+                **Hover Data**
 
+                Mouse over values in the graph.
+            """),
+            html.Pre(id='hover-data')
+        ], className='three columns')
+],)
+
+@app.callback(
+    Output('hover-data', 'children'),
+    [Input('basic-interactions', 'hoverData')])
+def display_click_data(hoverData):
+    return json.dumps(hoverData, indent=2)
 if __name__ == '__main__':
     app.run_server(debug=True)
